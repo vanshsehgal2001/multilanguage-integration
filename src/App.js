b5e74f4079bfc92c366360f32a153c6dd10c3d4d
vanshsehgal2001 <rockstarvansh2001@gmail.com>
2021-12-07 20:36:04 +0530

    "first commit"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {useTranslation} from 'react-i18next'
+import i18next from 'i18next'
+import './App.css'
+import cookies from 'js-cookie'
 
-function App() {
+const languages = [
+  {
+    id:'en',
+    name:'English'
+  },
+  {
+    id:'hi',
+    name:'हिंदी'
+  },
+  {
+    id:'ar',
+    name:'عربي'
+  },
+  {
+    id:'bn',
+    name:'বাংলা'
+  },
+  {
+    id:'es',
+    name:'Español'
+  },
+  {
+    id:'fr',
+    name:'français'
+  },
+  {
+    id:'id',
+    name:'bahasa Indonesia'
+  },
+  {
+    id:'pt',
+    name:'português'
+  },
+  {
+    id:'ru',
+    name:'русский'
+  },
+  {
+    id:'zh',
+    name:'俄语'
+  },
+]
+
+const App = () => {
+  const code = cookies.get('i18next') || 'en'
+  const { t } = useTranslation();
+
+  const onclick = (id)=>{
+    console.log(id)
+    i18next.changeLanguage(id)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" >
+      <div>
+      <ul>
+        {languages.map(({id,name})=>{
+          return(
+            <li key={id} >
+              <button onClick={()=> onclick(id)} disabled={id === code } > {name}  </button>  
+            </li>
+          )
+        })}
+      </ul>
+      </div>
+      <div>
+      <h2>{t('Greeting Message')}</h2>
+
+      </div>
     </div>
   );
+
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React,{Suspense} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import i18n from "i18next";
+import {initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+i18n
+  .use(initReactI18next) 
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    lng: document.querySelector('html').lang, 
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false 
+    },
+    detection:{
+      order: ['cookie','htmlTag','path', 'cookie', 'localStorage', 'sessionStorage'],
+      caches:['cookie']
+    },
+    backend:{
+      loadPath: '/languages/{{lng}}/translation.json',
+    },
+    supportedLngs:['en','hi','zh','es','fr','ar','bn','ru','pt','id']
+  });
+
+  const markup=(
+    <h5>
+      Loading....
+    </h5>
+  )
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Suspense fallback={markup} >
+    <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Suspense>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
